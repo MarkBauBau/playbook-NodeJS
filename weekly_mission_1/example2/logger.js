80e8ee7b23fd9762aab34086a4b4f88f0f6afd10
MarkBauBau <markjr1201mx@gmail.com>
2022-04-22 20:17:41 -0500

    Paso 2: Agregando example2 con logger.js y main.js

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,17 @@
+// logger.js
+
+// Esta es una función que se guardara en este módulo como info
+exports.info = (message) => {
+  console.log(`info: ${message}`)
+}
+
+// Esta es una función que se guardara en este módulo como verbose
+exports.verbose = (message) => {
+  console.log(`verbose: ${message}`)
+}
+
+/* Código que se ejecuta en main.js
+  const logger = require('./logger')
+  logger.info('This is an informational message')
+  logger.verbose('This is a verbose message')
+ * */
